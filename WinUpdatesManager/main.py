7674242d9246ee6a525e38c9e8331e974060fa28
:Sandy <none@none>
2013-07-20 18:57:59 +0300

    [WinUpdatesManager] add attributes for main file, like from command prompt and check is this realy start instent not import. In this commit real work commented.

--- WinUpdatesManager/main.py
+++ WinUpdatesManager/main.py
@@ -1,29 +1,49 @@
+import sys
 import core
+import core.dates
+import core.dirs
+import core.updates
 
 
 def getUpdatesFromPackage(aFiles, aDate):
 
-    print('\n' + str(aDate) + '\n')
     updates = core.updates.getUpdatesInfoFromPackage(aFiles)
 
     for up in updates:
-        up = up[:len(up) - 1] + str(aDate) + '}'
+        up = up[:len(up) - 1] + ', ' + str(aDate) + '}'
         print(up)
 
 
 def getFromYearEditionPackage(aPaths, aDates):
 
+    if len(aDates) == 0 or len(aPaths) == 0:
+        return
+
     i = 0
     for path in aPaths:
-        files = core.updates.getSubDirectoryFiles(path)
-
-        for fileName in files:
-            fileName = fileName[fileName.find('KB'):]
-
-        core.updates.getUpdatesFromPackageToJSON(files,
-            aDates[min(i, len(aDates) - 1)])
+        files = core.dirs.getSubDirectoryFiles(path)
+        getUpdatesFromPackage(files, aDates[min(i, len(aDates) - 1)])
         i += 1
 
 
-#getFromYearEditionPackage(getSubFolderOnly('D:\\', True),
-#getDatesForYearEdition(getSubFolderOnly('D:\\')))
+if __name__ == '__main__':
+
+    argc = len(sys.argv)
+    if argc == 1 or argc > 3:
+
+        print('Bad using.\n'
+              'Correct will be ' + sys.argv[0] +
+              ' <path to directory with updates> <date for non Year edition>')
+    elif argc == 2:
+
+        print('One ' + sys.argv[1])
+        #rootFolders = core.dirs.getSubFolderOnly(sys.argv[1])
+        #dates = core.dates.getDatesOfUpdates(rootFolders)
+        #if len(dates) == 0:
+        #    return
+        #rootFolders = core.dirs.getSubFolderOnly(sys.argv[1], True)
+        #getFromYearEditionPackage(rootFolders, dates)
+
+    elif argc == 3:
+        print('Two ' + sys.argv[1] + ' ' + sys.argv[2])
+        #getUpdatesFromPackage(sys.argv[1], sys.argv[2])
