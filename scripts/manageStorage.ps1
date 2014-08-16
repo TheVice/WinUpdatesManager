fca3b8d99cf263d9d894171b3a6dcfeb16b104f9
user <user@eight.pc>
2014-08-16 16:16:16 +0300

    [PowerShell] added MountImage2 that mount ISO images without drive letter - only volume ID is present in that case, that are returned. Also added mklink and rmlink form linking and unlinking structure of folders.

--- scripts/manageStorage.ps1
+++ scripts/manageStorage.ps1
@@ -44,11 +44,39 @@ Function MountImages([string[]]$aImagePaths)
 	$mountPoints
 }
 
+Function MountImages2([string[]]$aImagePaths)
+{
+	[string[]]$volumeIds = @()
+	For ($i = 0; $i -lt $aImagePaths.Count; $i++)
+	{
+		$volumeIds += (Mount-DiskImage -ImagePath $aImagePaths[$i] -StorageType ISO -NoDriveLetter -PassThru | Get-Volume).ObjectId
+	}
+	$volumeIds
+}
+
 Function UnMountImages([string[]]$aImagePaths)
 {
 	For ($i = 0; $i -lt $aImagePaths.Count; $i++)
 	{
 		Dismount-DiskImage -ImagePath $aImagePaths[$i]
+	}
+}
+
+#http://stackoverflow.com/questions/894430/powershell-hard-and-soft-links#comment9823010_5549583
+Function mklink { cmd /c mklink $args }
+
+Function rmlink($aPath)
+{
+	If (Test-Path $aPath)
+	{
+		If(Test-Path $aPath -PathType Container)
+		{
+			cmd /c rd $aPath
+		}
+		Else
+		{
+			cmd /c del $aPath
+		}
 	}
 }
 
@@ -59,4 +87,13 @@ Function UnMountImages([string[]]$aImagePaths)
 #Write-Output($addons)
 
 #Write-Output(MountImages($isos))
+#UnMountImages($isos)
+
+#mklink /J C:\FileLink.txt C:\FileName.txt
+#mklink C:\FolderLink C:\Folder
+
+#rmlink C:\FolderLink
+#rmlink C:\FileLink.txt
+
+#Write-Output(MountImages2($isos))
 #UnMountImages($isos)
