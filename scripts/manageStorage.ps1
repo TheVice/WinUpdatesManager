1d1ed4e223af5fe33f46ca4176524db7b6f8df43
user <user@eight.pc>
2014-08-16 18:52:01 +0300

    [PowerShell] added GetFolderNameForImage, MountVolumeOfImage and UnMountVolumeOfImage. First function prepare name for folder to which mount the mounted image content. MountVolumeOfImage - mount the Volume with no mount point into empty folder. UnMountVolumeOfImage - remove mount point of Volume by know mount point.

--- scripts/manageStorage.ps1
+++ scripts/manageStorage.ps1
@@ -62,6 +62,73 @@ Function UnMountImages([string[]]$aImagePaths)
 	}
 }
 
+Function GetFolderNameForImage($aImagePath)
+{
+	$folderName = ""
+	$beginIndex = $aImagePath.LastIndexOf("-") + 1
+	$endIndex = $aImagePath.LastIndexOf(".")
+	$len = $endIndex - $beginIndex
+
+	If ($len -eq 6) #Windows-KB913086-201101.iso
+	{
+		$folderName = $aImagePath.Substring($beginIndex, $len)
+	}
+	Else #Windows-KB913086-201102-1.iso
+	{
+		$baseName = $aImagePath.Substring(0, $beginIndex - 1)
+		$baseName += "."
+		$baseName = GetFolderNameForImage($baseName)
+		$additionName = $aImagePath.Substring($beginIndex - 1, $len + 1)
+		$folderName = $baseName + $additionName
+	}
+	$folderName
+}
+
+Function MountVolumeOfImage()
+{
+	[string[]]$mountedFolders = @()
+	$rootPath = $args[0][0]
+	[string[]]$imagePaths = $args[0][1]
+	[string[]]$volumeIds = $args[0][2]
+
+	If ((Test-Path $rootPath -PathType Container) -and ($imagePaths.Count -eq $volumeIds.Count))
+	{
+		For ($i = 0; $i -lt $imagePaths.Count; $i++)
+		{
+			$folderPath = $rootPath + "\" + (GetFolderNameForImage $imagePaths[$i])
+			If ((Test-Path $folderPath) -eq $false)
+			{
+				Write-Output("Create folder - " + (New-Item -Path $folderPath -ItemType Directory).FullName)
+			}
+			If ((Test-Path $folderPath -PathType Container) -and ((Get-ChildItem $folderPath) -eq $null))
+			{
+				mountvol $folderPath $volumeIds[$i]
+				If ((Get-ChildItem $folderPath) -ne $null)
+				{
+					$mountedFolders += $folderPath
+				}
+				Else
+				{
+					Write-Output("mountvol not mounted image " + $imagePaths[$i] + " with Volume id " + $volumeIds[$i] + " to folder " + $folderPath)
+				}
+			}
+			Else
+			{
+				Write-Output("Incorect condition - " + $folderPath + ": path set to file or path to non empty folder")
+			}
+		}
+	}
+	$mountedFolders
+}
+
+Function UnMountVolumeOfImage($aMountedFolders)
+{
+	For ($i = 0; $i -lt $aMountedFolders.Count; $i++)
+	{
+		mountvol $aMountedFolders[$i] /D
+	}
+}
+
 #http://stackoverflow.com/questions/894430/powershell-hard-and-soft-links#comment9823010_5549583
 Function mklink { cmd /c mklink $args }
 
@@ -96,4 +163,18 @@ Function rmlink($aPath)
 #rmlink C:\FileLink.txt
 
 #Write-Output(MountImages2($isos))
+#UnMountImages($isos)
+
+#$rootPath = "C:\Public"
+#[string[]]$isos = @()
+#[string[]]$volumes = @()
+#$isos += "C:\Windows-KB913086-201101.iso"
+#$isos += "C:\Windows-KB913086-201102-1.iso"
+
+#$volumes += MountImages2($isos)
+#$mountedPoints = MountVolumeOfImage($rootPath, $isos, $volumes)
+
+#Write-Output("Mounted points - " + $mountedPoints)
+
+#UnMountVolumeOfImage($mountedPoints)
 #UnMountImages($isos)
